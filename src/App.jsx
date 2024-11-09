7e57954ac6cdc874bccb3920b82b0470b5bf31df
Halil Yeşilyurt <haliilyesilyurt@gmail.com>
2024-11-10 00:22:57 +0300

    add GameBoard component for rendering Tic Tac Toe grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import { useState } from 'react';
 import Player from './components/Player.jsx';
+import GameBoard from './components/GameBoard.jsx';
 
 const PLAYERS = {
   X: 'Player 1',
   O: 'Player 2',
 };
+
+const INITIAL_BOARD = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function getActivePlayer(gameTurns) {
+  return gameTurns.length % 2 === 0 ? 'X' : 'O';
+}
+
+function generateGameBoard(gameTurns) {
+  const gameBoard = INITIAL_BOARD.map((row) => [...row]);
+
+  for (const { square, player } of gameTurns) {
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
 
 function App() {
   const [players, setPlayers] = useState(PLAYERS);
@@ -21,6 +43,7 @@ function App() {
         <ol>
           <Player initialName={PLAYERS.X} symbol='X' isActive={activePlayer === 'X'} onChangeName={handlePlayersName} />
           <Player initialName={PLAYERS.O} symbol='O' isActive={activePlayer === 'O'} onChangeName={handlePlayersName} />
+          <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare} />
         </ol>
       </div>
     </main>
