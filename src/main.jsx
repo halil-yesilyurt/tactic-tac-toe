0c0bbbe0869ff2efbdeddf08a1de70c69c11fb33
Halil Yeşilyurt <haliilyesilyurt@gmail.com>
2025-05-06 13:40:58 +0300

    refactor: set background image as a CSS variable for improved maintainability

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import bgImage from './assets/tic-tac-toe.png';
+
+// Set the background image as a CSS variable
+document.documentElement.style.setProperty('--bg-image', `url(${bgImage})`);
 
 createRoot(document.querySelector('body')).render(
   <StrictMode>
